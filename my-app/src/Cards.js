6ce427f1d51f4c993ce4f0b0bdf016ced3491888
Miley Wright <dishaliekwright@yahoo.com>
2020-02-11 14:26:07 -0500

    props are passed from API

--- my-app/src/Cards.js
+++ my-app/src/Cards.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css'
 
 const Cards = props => {
 console.log(props.user)
     return(
-        <>
+        <div className= 'userContainer'>
         <h2> {props.user.name}</h2>
-        </>
+        <img width='200' src={props.user.avatar_url} alt={props.user.name} />
+        </div>
     )
 }
 
